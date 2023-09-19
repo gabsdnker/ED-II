d01f77c609ee292d7e36d916d6051834068869e7
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2023-09-19 12:26:38 -0300

    Update GD-EDII-Lista-2.py

--- GD-EDII-Lista-2.py
+++ GD-EDII-Lista-2.py
@@ -1,56 +1,86 @@
 #Nome: Gabrielli Danker     Matéria: Estrutura de Dados II 
 
+from collections import deque
+
 class GrafoNaoDirigido:
     def __init__(self):
         self.vertices = {}  # Um dicionário onde as chaves são vértices e os valores são listas de vértices adjacentes.
-    
+
     def insereV(self, v):
         if v not in self.vertices:
             self.vertices[v] = []
-    
+
     def removeV(self, v):
         if v in self.vertices:
             for adjacente in self.vertices[v]:
                 self.vertices[adjacente].remove(v)
             del self.vertices[v]
-    
+
     def insereA(self, u, v):
         if u in self.vertices and v in self.vertices:
             if v not in self.vertices[u]:
                 self.vertices[u].append(v)
             if u not in self.vertices[v]:
                 self.vertices[v].append(u)
-    
+
     def removeA(self, u, v):
         if u in self.vertices and v in self.vertices:
             if v in self.vertices[u]:
                 self.vertices[u].remove(v)
             if u in self.vertices[v]:
                 self.vertices[v].remove(u)
-    
+
     def adj(self, v):
         return self.vertices[v]
-    
+
     def getA(self, u, v):
         if u in self.vertices and v in self.vertices:
             if v in self.vertices[u]:
                 return (u, v)
         return None
-    
+
     def grau(self, v):
         return len(self.vertices[v])
-    
+
     def verticesA(self, e):
         u, v = e
         return (u, v)
-    
+
     def arestas(self):
         arestas = []
         for v, adjacentes in self.vertices.items():
             for adjacente in adjacentes:
                 arestas.append((v, adjacente))
         return arestas
-    
+
+    def bfs(self, inicio):
+        visitados = {}  # Dicionário para rastrear os vértices visitados
+        fila = deque()  # Fila para BFS
+        fila.append(inicio)  # Começamos a busca a partir do vértice inicial
+
+        while fila:
+            vertice = fila.popleft()
+            if vertice not in visitados:
+                visitados[vertice] = True
+                print("Visitando:", vertice)
+
+                # Enfileira todos os vizinhos não visitados
+                for vizinho in self.adj(vertice):
+                    if vizinho not in visitados:
+                        fila.append(vizinho)
+
+    def dfs(self, vertice, visitados=None):
+        if visitados is None:
+            visitados = {}  # Dicionário para rastrear os vértices visitados
+        if vertice not in visitados:
+            visitados[vertice] = True
+            print("Visitando:", vertice)
+
+            # Chama recursivamente a DFS nos vizinhos não visitados
+            for vizinho in self.adj(vertice):
+                if vizinho not in visitados:
+                    self.dfs(vizinho, visitados)
+
     def __str__(self):
         result = "Vértices:\n"
         for vertex in self.vertices:
@@ -60,53 +90,6 @@ class GrafoNaoDirigido:
             result += f"{edge}\n"
         return result
 
-    def bfs(self, inicio):
-        visitados = {}
-        fila = []
-        for v in self.vertices:
-            visitados[v] = False
-        fila.append(inicio)
-        visitados[inicio] = True
-
-        while fila:
-            vertice = fila.pop(0)
-            print(vertice, end=" -> ")
-            for vizinho in self.adj(vertice):
-                if not visitados[vizinho]:
-                    fila.append(vizinho)
-                    visitados[vizinho] = True
-        print("Fim")
-
-    def dfs(self, inicio):
-        visitados = {}
-        for v in self.vertices:
-            visitados[v] = False
-        self._dfs_recursivo(inicio, visitados)
-
-    def _dfs_recursivo(self, vertice, visitados):
-        print(vertice, end=" -> ")
-        visitados[vertice] = True
-        for vizinho in self.adj(vertice):
-            if not visitados[vizinho]:
-                self._dfs_recursivo(vizinho, visitados)
-                
-    def imprime_caminho(self, inicio, fim):
-        visitados = {}
-        caminho = []
-        for v in self.vertices:
-            visitados[v] = False
-        self._encontra_caminho(inicio, fim, visitados, caminho)
-        print("Caminho de", inicio, "a", fim, ":", caminho)
-
-    def _encontra_caminho(self, atual, fim, visitados, caminho):
-        visitados[atual] = True
-        caminho.append(atual)
-        if atual == fim:
-            return
-        for vizinho in self.adj(atual):
-            if not visitados[vizinho]:
-                self._encontra_caminho(vizinho, fim, visitados, caminho)
-
 def main():
     grafo = GrafoNaoDirigido()
     grafo.insereV('A')
@@ -114,7 +97,6 @@ def main():
     grafo.insereV('C')
     grafo.insereV('D')
     grafo.insereV('E')
-
     grafo.insereA('A', 'B')
     grafo.insereA('B', 'C')
     grafo.insereA('C', 'D')
@@ -126,16 +108,14 @@ def main():
     print("Grau de 'D':", grafo.grau('D'))  # Saída: 1
     print("Arestas do grafo:", grafo.arestas())  # Saída: [('A', 'B'), ('B', 'A'), ('B', 'C'), ('C', 'B'), ('C', 'D'), ('C', 'E'), ('D', 'C'), ('E', 'C')]
 
-    print(grafo)  # Saída formatada do grafo
-
     print("Busca em Largura:")
-    grafo.bfs('A')  # Exemplo de BFS a partir do vértice 'A'
+    grafo.bfs('A')
 
-    print("\nBusca em Profundidade:")
-    grafo.dfs('A')  # Exemplo de DFS a partir do vértice 'A'
+    print("Busca em Profundidade:")
+    grafo.dfs('A')
 
-    print("\nCaminho entre 'A' e 'D':")
-    grafo.imprime_caminho('A', 'D')  # Exemplo de impressão de caminho entre 'A' e 'D'
+    print(grafo)  # Saída formatada do grafo
 
 if __name__ == "__main__":
     main()
+
